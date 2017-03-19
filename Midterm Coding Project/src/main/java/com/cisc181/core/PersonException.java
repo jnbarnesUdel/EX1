10ebcab85bf1a37bcffcf5c605370fe6bb304c2c
Patrick <Patrick@128.4.95.53>
2017-03-19 19:51:05 -0400

    jon first ex done

---
+++ Midterm Coding Project/src/main/java/com/cisc181/core/PersonException.java
@@ -0,0 +1,23 @@
+package com.cisc181.core;
+
+import java.util.Date;
+import java.util.regex.Pattern;
+
+public class PersonException extends Exception 
+{
+	Person Person;
+	String regex = "^\\(?([0-9]{3})\\)?[-.\\s]?([0-9]{3})[-.\\s]?([0-9]{4})$";
+	public PersonException(String message)
+	{
+		super(message);
+	}
+	if(Person.PrintAge()<100)
+	{
+	}
+	else
+	{
+		throw new PersonException("Age over 100");
+	}
+	
+	Pattern pattern = 
+}
