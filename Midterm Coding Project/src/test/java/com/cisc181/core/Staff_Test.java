10dfe02b121fca8013ee8bc6ea6d5c3c1b7bebcf
Patrick <Patrick@128.4.95.53>
2017-03-19 21:54:57 -0400

    jon did test cases

--- Midterm Coding Project/src/test/java/com/cisc181/core/Staff_Test.java
+++ Midterm Coding Project/src/test/java/com/cisc181/core/Staff_Test.java
@@ -4,9 +4,13 @@ import static org.junit.Assert.*;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import com.cisc181.eNums.eNewTitle;
 
 public class Staff_Test {
 
@@ -16,7 +20,50 @@ public class Staff_Test {
 	
 	@Test
 	public void test() {
+		Date d1 = new Date(1997, 11, 21);
+		Date d2 = new Date(2011, 10, 14);
+		Staff s1 = new Staff("Jon", "doe", "Doe", d1, "Jo strt", "7172401375", "JonDoe@udel.edu",
+				"7 to 9", 12, 40000.00, d2, eNewTitle.Dr);
+		Staff s2 = new Staff("Jon", "doe", "Doe", d1, "Jo strt", "7172401375", "JonDoe@udel.edu",
+				"7 to 9", 12, 50000.00, d2, eNewTitle.Dr);
+		Staff s3 = new Staff("Jon", "doe", "Doe", d1, "Jo strt", "7172401375", "JonDoe@udel.edu",
+				"7 to 9", 12, 460000.00, d2, eNewTitle.Dr);
+		Staff s4 = new Staff("Jon", "doe", "Doe", d1, "Jo strt", "7172401375", "JonDoe@udel.edu",
+				"7 to 9", 12, 140000.00, d2, eNewTitle.Dr);
+		Staff s5 = new Staff("Jon", "doe", "Doe", d1, "Jo strt", "7172401375", "JonDoe@udel.edu",
+				"7 to 9", 12, 240000.00, d2, eNewTitle.Dr);
+		
+		ArrayList<Staff> alist = new ArrayList<>();
+		alist.add(s1);
+		alist.add(s2);
+		alist.add(s3);
+		alist.add(s4);
+		alist.add(s5);
+		
+		int total = 0;
+		for(Staff s : alist)
+		{
+			total = total + (int)s.getSalary();
+		}
+		int av = total / 5;
+		assertEquals(av, 186000);
 		assertEquals(1,1);
+		
 	}	
+	
+	@Test(expected = PersonException.class)
+	public void testPhoneNumException() throws PersonException {
+		Date d1 = new Date(1997, 11, 21);
+		Date d2 = new Date(2011, 10, 14);
+		// wrong phone number
+		Staff s1 = new Staff("Jon", "doe", "Doe", d1, "Jo strt", "7", "JonDoe@udel.edu",
+				"7 to 9", 12, 40000.00, d2, eNewTitle.Dr);
+		//over 100 years ago DOB
+		Date d3 = new Date(97, 11, 21);
+		Staff s2 = new Staff("Jon", "doe", "Doe", d3, "Jo strt", "7172401375", "JonDoe@udel.edu",
+				"7 to 9", 12, 40000.00, d2, eNewTitle.Dr);
+	// Run some code that will throw PersonException.class
+	// This test will fail if that exception ISN'T thrown...!
+	}
 
 }
--- Midterm Coding Project/src/test/java/com/cisc181/core/Student_Test.java
+++ Midterm Coding Project/src/test/java/com/cisc181/core/Student_Test.java
@@ -4,18 +4,93 @@ import static org.junit.Assert.*;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import com.cisc181.eNums.eMajor;
 
 public class Student_Test {
 
 	@BeforeClass
 	public static void setup() {
+		Course c1 = new Course();
+		Course c2 = new Course();
+		Course c3= new Course();
+		ArrayList<Course> courseRecord = new ArrayList<>();
+		courseRecord.add(c1);
+		courseRecord.add(c2);
+		courseRecord.add(c3);
+		
+		Semester f1 =  new Semester();
+		Semester sp1 = new Semester();
+		ArrayList<Semester> semesterRecord = new ArrayList<>();
+		semesterRecord.add(f1);
+		semesterRecord.add(sp1);
+		
+		Section s1 = new Section();
+		Section s2 = new Section();
+		Section s3 = new Section();
+		Section s4 = new Section();
+		Section s5 = new Section();
+		Section s6 = new Section();
+		ArrayList<Section> sectionRecord = new ArrayList<>();
+		sectionRecord.add(s1);
+		sectionRecord.add(s2);
+		sectionRecord.add(s3);
+		sectionRecord.add(s4);
+		sectionRecord.add(s5);
+		sectionRecord.add(s6);
+		
+		Date d1 = new Date(1997, 11, 11);
+		Student st1 = new Student("John", "Doe", "Doe", d1, eMajor.BUSINESS, "SmithSt", "7172401375", "JohnDoe@udel.edu");
+		Student st2 = new Student("John", "Doe", "Doe", d1, eMajor.BUSINESS, "SmithSt", "7172401375", "JohnDoe@udel.edu");
+		Student st3 = new Student("John", "Doe", "Doe", d1, eMajor.BUSINESS, "SmithSt", "7172401375", "JohnDoe@udel.edu");
+		Student st4 = new Student("John", "Doe", "Doe", d1, eMajor.BUSINESS, "SmithSt", "7172401375", "JohnDoe@udel.edu");
+		Student st5 = new Student("John", "Doe", "Doe", d1, eMajor.BUSINESS, "SmithSt", "7172401375", "JohnDoe@udel.edu");
+		Student st6 = new Student("John", "Doe", "Doe", d1, eMajor.BUSINESS, "SmithSt", "7172401375", "JohnDoe@udel.edu");
+		Student st7 = new Student("John", "Doe", "Doe", d1, eMajor.BUSINESS, "SmithSt", "7172401375", "JohnDoe@udel.edu");
+		Student st8 = new Student("John", "Doe", "Doe", d1, eMajor.BUSINESS, "SmithSt", "7172401375", "JohnDoe@udel.edu");
+		Student st9 = new Student("John", "Doe", "Doe", d1, eMajor.BUSINESS, "SmithSt", "7172401375", "JohnDoe@udel.edu");
+		Student st10 = new Student("John", "Doe", "Doe", d1, eMajor.BUSINESS, "SmithSt", "7172401375", "JohnDoe@udel.edu");
+		ArrayList<Student> studentRecord = new ArrayList<>();
+		studentRecord.add(st1);
+		studentRecord.add(st2);
+		studentRecord.add(st3);
+		studentRecord.add(st4);
+		studentRecord.add(st5);
+		studentRecord.add(st6);
+		studentRecord.add(st7);
+		studentRecord.add(st8);
+		studentRecord.add(st9);
+		studentRecord.add(st10);
+
 	}
 
 	@Test
 	public void test() {
+		double g = 80.0;
+		for(Section s : sectionRecord)
+		{
+			for(Student st : studentRecord)
+			{
+				Enrollment e = new Enrollment(s.getSectionID(), st.getStudentID());
+				e.setGrade(g);
+				ArrayList<Enrollment> enrollmentRecord = new ArrayList<>();
+			}
+		}
+		
+		double av = 0;
+		for(Enrollment e : enrollmentRecord)
+		{
+			av = av + e.getGrade();
+		}
+		av = av / 60;
+		assertEquals(av, 80);	//this will be av for all because g = 80 for all
+		
+		studentRecord.get(1).setMajor(eMajor.CHEM);
+		
 		assertEquals(1, 1);
 	}
 }
